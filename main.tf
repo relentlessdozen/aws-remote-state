38a57a1e8bc2a2f6df03dd220d38c3126441a572
Rick James <patrickjamesjr@protonmail.com>
2020-01-31 15:54:32 -0500

    Commit after addition of main.tf!

---
+++ main.tf
@@ -0,0 +1,32 @@
+# Provider declaration
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "pack-pride-remote-state"
+  # Enable versioning so we can see the full revision history of our state
+  versioning {
+    enabled = true
+  }
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# Define Dynamo DB table for state locking
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "tf-state-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
